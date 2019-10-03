9c9bc4c2f9212f80747a5f400350a303ee180fe0
Tên của bạn <lamquangphuc1996@gmail.com>
2019-10-03 16:02:29 +0800

    a

--- LearnJava8/src/com/omi/datetime/common.java
+++ LearnJava8/src/com/omi/datetime/common.java
@@ -3,8 +3,11 @@ package com.omi.datetime;
 public class common {
 	public void show(String name)
 	{
-		 String a ="Phuc";
-		 System.out.println(""+a);
+		 String b ="Phuc";
+		 System.out.println(""+b);
+		 System.out.println(b);
+		 System.out.println(""+b);
+		 System.out.println(""+b);System.out.println(b);
 	}
 
 }
